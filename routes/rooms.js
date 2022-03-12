4c628188e244e1a7d343905bb0c85084785e676a
기웅민 <kiwoongmin@giungmin-ui-MacBookPro.local>
2022-03-13 05:09:58 +0900

    chore: 깃 푸시 안 되어서 남긴 주석 제거

--- routes/rooms.js
+++ routes/rooms.js
@@ -7,7 +7,6 @@ const {
   getRoomPeople,
 } = require("./controllers/rooms.controller");
 
-// 깃 커밋 적용 안 되서 확인차 남기는 주석. 지울 것임.
 router.get("/:id", verifyToken, getRoomInfo);
 router.get("/:id/game", verifyToken, getRoomPeople);
 router.post("/:id", verifyToken, registerRoom);
